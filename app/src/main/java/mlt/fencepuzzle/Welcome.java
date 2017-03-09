b3b998c4056dadfa8bad8ec98caa6821530d6aab
Michael Nguyen <michaelnguyenm@gmail.com>
2017-03-09 11:55:15 -0600

    Added forgotten strings.xml and added data members to Welcome

--- app/src/main/java/mlt/fencepuzzle/Welcome.java
+++ app/src/main/java/mlt/fencepuzzle/Welcome.java
@@ -2,8 +2,15 @@ package mlt.fencepuzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+
+    // Buttons
+    private Button mStartButton;
+    private Button mInstructionButton;
+    private Button mOptionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
