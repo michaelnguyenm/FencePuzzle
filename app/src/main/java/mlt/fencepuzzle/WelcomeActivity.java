efb03006e688b51509780633f84956b36d7012e9
TimCoding <timbvho@gmail.com>
2017-03-17 21:11:19 -0500

    Created the difficulty level selector activity (LevelSelectorActivity.java, activity_level_selector.xml) and modified WelcomeActivity.java and activity_welcome.xml so that selecting Start will send you to the level selector activity

--- app/src/main/java/mlt/fencepuzzle/WelcomeActivity.java
+++ app/src/main/java/mlt/fencepuzzle/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package mlt.fencepuzzle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -16,5 +18,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void chooseLevel(View view){
+        Intent intent = new Intent(this, LevelSelectorActivity.class);
+        startActivity(intent);
     }
 }
