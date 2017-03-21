4f4767d847c416ed0c16f28bd3e463f5f3c7ad03
Michael Nguyen <michaelnguyenm@gmail.com>
2017-03-21 11:52:15 -0500

    Trimmed SettingsActivity and made WelcomeActivity buttons work

--- app/src/main/java/mlt/fencepuzzle/WelcomeActivity.java
+++ app/src/main/java/mlt/fencepuzzle/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package mlt.fencepuzzle;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,9 @@ public class WelcomeActivity extends AppCompatActivity {
     private Button mInstructionButton;
     private Button mOptionButton;
 
+    // Settings
+    private static int SETTINGS_REQUEST = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,27 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SETTINGS_REQUEST) {
+            SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+        }
+    }
+
     public void chooseLevel(View view) {
         Intent intent = new Intent(this, LevelSelectorActivity.class);
         startActivity(intent);
     }
+
+    public void viewInstructions(View view) {
+        Intent intent = new Intent(this, InstructionsActivity.class);
+        startActivity(intent);
+    }
+
+    public void changeSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, SETTINGS_REQUEST);
+    }
+
+
 }
